7d7daa749ccfed8ee3c2c96d06b9f4e0a6da12b2
Pichaipusit Kamsorn <Pichaipusit.ks@gmail.com>
2024-11-23 21:47:11 +0700

    prob: Add page to load more data

---
+++ yo.js
@@ -0,0 +1,7 @@
+// 🎯 Target: You’re working on an art marketplace app that lets the user submit multiple orders for an art item at the same time.
+const t1 = 1;
+
+// 🧀 Here
+let na = "ni";
+
+// 🧀 Here
